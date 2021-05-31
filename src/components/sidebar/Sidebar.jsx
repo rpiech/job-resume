efea432b052c7426b690d9193573923335bb6e2e
Rafal <r.piech@lightbeam.com>
2021-05-31 15:31:32 +0200

    add router, add experience page, add crimson to headings

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,24 +1,30 @@
 import "./sidebar.scss"
+import { Link } from 'react-router-dom'
 
 export default function Sidebar({openMenu, setOpenMenu}) {
   return (
     <div className={"sidebar " + (openMenu && "active")}>
       <ul className="sidebar__list">
         <li onClick={() => setOpenMenu(false)}>
-          <a href="#">Home</a>
+          <a href="/#">Home</a>
         </li>
         <li onClick={() => setOpenMenu(false)}>
-        <a href="#about">About me</a>
+        <a href="/#about">About me</a>
         </li>
         <li onClick={() => setOpenMenu(false)}>
-          <a href="#work">Work Experience</a>
+          <a href="/#work">Work</a>
         </li>
         <li onClick={() => setOpenMenu(false)}>
-          <a href="#hobby">Hobbies</a>
+          <a href="/#hobby">Hobbies</a>
         </li>
         <li onClick={() => setOpenMenu(false)}>
-          <a href="#footer">Contact</a>
+          <a href="/#footer">Contact</a>
         </li>
+        <Link to="/experience">
+          <li onClick={() => setOpenMenu(false)}>
+            <a href="/experience">Experience</a>
+          </li>
+        </Link>
       </ul>
     </div>
   )
